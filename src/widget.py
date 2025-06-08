498d890abc0516a4d932f64a7358e0e587a1bec4
chelchik91 <chelchik91@mail.ru>
2025-06-08 22:39:50 +0700

    mask_account_card in widget.py ready

--- src/widget.py
+++ src/widget.py
@@ -2,4 +2,17 @@ from masks import get_mask_card_number, get_mask_account
 
 
 def mask_account_card(type_account_card_number: str) -> str:
-    pass
+    res_list = type_account_card_number.split()
+    number = res_list[-1]
+
+    if res_list[0] != 'Счет':
+        res_list[-1] = get_mask_card_number(number)
+    else:
+        res_list[-1] = get_mask_account(number)
+
+    return ' '.join(res_list)
+
+
+if __name__ == '__main__':
+    print(mask_account_card('Visa Platinum 7000792289606361'))
+    print(mask_account_card('Счет 73654108430135874305'))
