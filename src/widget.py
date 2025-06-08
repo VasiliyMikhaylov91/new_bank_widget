b587d436451cb2d55434ce67d1453aeef45917cd
chelchik91 <chelchik91@mail.ru>
2025-06-08 22:56:03 +0700

    add get_date in widget.py

--- src/widget.py
+++ src/widget.py
@@ -13,6 +13,13 @@ def mask_account_card(type_account_card_number: str) -> str:
     return ' '.join(res_list)
 
 
+def get_date(date_and_time: str) -> str:
+    date = date_and_time.split('T')[0].split('-')
+    date.reverse()
+    return '.'.join(date)
+
+
 if __name__ == '__main__':
     print(mask_account_card('Visa Platinum 7000792289606361'))
     print(mask_account_card('Счет 73654108430135874305'))
+    print(get_date("2024-03-11T02:26:18.671407"))
