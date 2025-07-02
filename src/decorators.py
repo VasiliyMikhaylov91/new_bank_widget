5396bbf47d183ede24866079c9073aef9aba0a5b
chelchik91 <chelchik91@mail.ru>
2025-07-02 23:14:59 +0700

    add test for decorators.py correct PEP8

--- src/decorators.py
+++ src/decorators.py
@@ -2,26 +2,30 @@ from functools import wraps
 from typing import Any
 
 
-def log(filename:str = '') -> Any:
-    '''
+def log(filename: str = "") -> Any:
+    """
     Декоратор указывает имя выполненной функции и результат выполнения
     В случае выполнения функции с ошибкой указывается текст ошибки и входные параметры функции
     При указании параметра filename записи будут вестись в указанный файл.
-    '''
+    """
 
     def wrapper(function: Any) -> Any:
         @wraps(function)
         def inner(*args: Any, **kwargs: Any) -> Any:
-            result = f'{function.__name__} '
+            result = f"{function.__name__} "
+            function_result = None
             try:
-                function(*args, **kwargs)
-                result += 'ok\n'
+                function_result = function(*args, **kwargs)
+                result += "ok\n"
             except Exception as e:
-                result += f'error: {e}. Inputs:{args}, {kwargs}\n'
+                result += f"error: {e}. Inputs:{args}, {kwargs}\n"
             if filename:
-                with open(filename, 'a') as file:
+                with open(filename, "a") as file:
                     file.write(result)
             else:
                 print(result)
+            return function_result
+
         return inner
+
     return wrapper
